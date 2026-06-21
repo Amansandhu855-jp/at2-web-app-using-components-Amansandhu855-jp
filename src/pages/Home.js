33967a4f2728fba9ce3fcc79eff78a163f2023d3
KAUR Amandeep <samratsandhu855@gmail.com>
2026-06-21 13:55:24 +0800

    Fixing typo error on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../compnents/Header";
+import Header from "../components/Header";
 import PropertyCard from "../components/PropertyCard";
 import properties from "../data/Properties";
 import ActivityCard from "../components/ActivityCard";
