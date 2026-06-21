79d0a411fd47049bad0a7c357ea8f660dbe6afb9
KAUR Amandeep <samratsandhu855@gmail.com>
2026-06-21 13:47:19 +0800

    fixing images path on properties.js

--- src/data/properties.js
+++ src/data/properties.js
@@ -4,7 +4,7 @@ const Properties = [
 		id: 0,
 		Title: "Beautiful Family Home",
 		location: "Rivendell",
-		image: "/images/properties/property1.jpg",
+		image: "images/properties/property1.jpg",
   		price: "$950",
   		bedrooms: 4,
   		bathrooms: 2
@@ -13,7 +13,7 @@ const Properties = [
 		"id": 1,
 		title: "Modern Apartment",
 		location: "Minas Tirith",
-		image: "/images/properties/property2.jpg",	
+		image: "images/properties/property2.jpg",	
 	    price: "$1300",
   		bedrooms: 3,
   		bathrooms: 2
@@ -22,7 +22,7 @@ const Properties = [
 		"id": 2,
 		title: "Cozy Cottage",
 		location: "Shire",
-		image: "/images/properties/property3.jpg",
+		image: "images/properties/property3.jpg",
 		price: "$560",
   		bedrooms: 1,
   		bathrooms: 2
@@ -31,7 +31,7 @@ const Properties = [
 		"id": 3,
 		title: "Luxury Villa",
 		location: "Hobbiton",
-		image: "/images/properties/property4.jpg",
+		image: "images/properties/property4.jpg",
 		price: "$85",
   		bedrooms: 2,
   		bathrooms: 2
@@ -40,7 +40,7 @@ const Properties = [
 		"id": 4,
 		title: "Charming Bungalow",
 		location: "Gondor",
-		image: "/images/properties/property5.jpg",	
+		image: "images/properties/property5.jpg",	
 		price: "$840",
   		bedrooms: 2,
   		bathrooms: 2
@@ -49,7 +49,7 @@ const Properties = [
 		"id": 5,
 		title: "Seaside Retreat",
 		location: "Mordor",
-		image: "/images/properties/property6.jpg",	
+		image: "images/properties/property6.jpg",	
 		price: "$150",
   		bedrooms: 2,
   		bathrooms: 2
