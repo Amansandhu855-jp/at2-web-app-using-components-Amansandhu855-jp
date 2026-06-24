4ce88ed3fe3c3933e6df1fef631d2b39cb40c744
KAUR Amandeep <samratsandhu855@gmail.com>
2026-06-24 09:45:11 +0800

    fixing reagins

--- src/pages/Regions.js
+++ src/pages/Regions.js
@@ -1,57 +1,25 @@
 import React from "react";
+import Regions from "../data/Regions";
 
-function Regions() {
-  const regions = [
-    {
-      name: "Rhovanion",
-    },
-
-    {
-      name: "Eriador",
-    },
-
-    {
-      name: "Bay of Belfalas",
-    },
-
-    {
-      name: "Mordor",
-    },
-
-    {
-      name: "The Southwest",
-    },
-
-    {
-      name: "Arnor",
-    },
-  ];
-
+function Region() {
+  
   return (
     <div>
+               <h1 className="section-title">
+                 Popular Regions
+               </h1>
 
-      <h1 className="section-title">
-        Popular Regions
-      </h1>
-
-      <div id="regions">
-
-        {regions.map((region, index) => (
-          <div className="item" key={index}>
-
-            <a href="#">
-
-              <h3>{region.name}</h3>
-
-            </a>
-
-          </div>
-        ))}
-
-      </div>
-
-    </div>
+             <div id="regions">
+              {Regions.map((region) => (
+             <div className="item" key={region.id}>
+              <a href="#">
+               <h3>{region.name}</h3>
+              </a>
+             </div>
+              ))}
+               </div>
+            </div>
   );
 }
 
-export default Regions;
+export default Region;
